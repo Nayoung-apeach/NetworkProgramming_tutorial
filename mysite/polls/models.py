48b3237d0f29641b335f894a48dc72561f918271
Nayoung-apeach <s2019w04@e-mirim.hs.kr>
2021-03-14 02:35:37 +0900

    polls/models Question, Choice

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)  # CASCADE 자동완성하면 생기는 소괄호 있으면 안된다.
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0) #ctrl + alt + l => 코드 포멧
+
+
